6b15ccdfff101c2e227478ec4df277cb743bac84
MinJ33 <erica990604@gmail.com>
2022-12-13 03:16:48 +0900

    feat: Add a function to close dropdown on click outside

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const basketStarterEl = document.querySelector('header .basket-starter')
 const basketEl = basketStarterEl.querySelector('.basket')
 
-basketStarterEl.addEventListener('click', function(){
+basketStarterEl.addEventListener('click', function(event){
+  event.stopPropagation()
   if (basketEl.classList.contains('show')){
     // if class 'show' exists in basketEl hide the basketEl
     basketEl.classList.remove('show')
@@ -11,3 +12,10 @@ basketStarterEl.addEventListener('click', function(){
   }
 })
 
+basketEl.addEventListener('click', function (event){
+  event.stopPropagation()
+})
+
+window.addEventListener('click', function(){
+    basketEl.classList.remove('show')
+})
