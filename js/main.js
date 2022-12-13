313b5760f9201595b4b7bf2e34f3895fa6e51708
MinJ33 <erica990604@gmail.com>
2022-12-13 15:54:33 +0900

    feat: Add showSearch and hideSearch function to hide navbar when search icon is clicked

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Basket !!
 const basketStarterEl = document.querySelector('header .basket-starter')
 const basketEl = basketStarterEl.querySelector('.basket')
 
@@ -25,4 +26,22 @@ function showBasket(){
 }
 function hideBasket(){
   basketEl.classList.remove('show')
+}
+
+// Search !!
+const headerEl = document.querySelector('header')
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter') 
+const searchCloserEl = searchWrapEl.querySelector('.search-closer') 
+const searchShadowEl = searchWrapEl.querySelector('.shadow') 
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchShadowEl.addEventListener('click', hideSearch)
+
+function showSearch() {
+  headerEl.classList.add('searching')
+}
+function hideSearch() {
+  headerEl.classList.remove('searching')
 }
